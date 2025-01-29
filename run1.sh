7530fd492ffa88382172437761a7641a6738607f
Sanjiv Kannaa Jeganathan <jsanjiv2003@gmail.com>
2025-01-29 11:07:07 +0530

    update queue numbers in .sh files

--- run1.sh
+++ run1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-arptables -A INPUT -j NFQUEUE --queue-num 69
+arptables -A INPUT -j NFQUEUE --queue-num 1
 echo "set arptables input rule"
 
 python3 main1.py
--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-arptables -A OUTPUT -j NFQUEUE --queue-num 70
+arptables -A OUTPUT -j NFQUEUE --queue-num 2
 echo "set arptables output rule"
 
 python3 main2.py
